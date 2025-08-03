d95bc5975c14a0692e6d0a919b2a7fffea094c84
George-83 <sair.grissom@gmail.com>
2025-08-03 19:54:17 +0300

    Implemented tests for first scenario

--- conftest.py
+++ conftest.py
@@ -1,5 +1,19 @@
 import pytest
+from utils.api_client import APIClient
 
+# Returns the base URL for the API. Used as the foundation for all API requests.
 @pytest.fixture
 def base_url():
     return "https://reqres.in"
+
+
+# Creates an instance of APIClient with the base URL. Allows sending HTTP requests to the API in tests
+@pytest.fixture
+def api_client(base_url):
+    return APIClient(base_url)
+
+
+# Performs a GET request to /api/users?page=1 endpoint. Returns the Response object for assertions in tests.
+@pytest.fixture
+def response(api_client):
+    return api_client.get("/api/users?page=1")
--- tests/test_01_list_users.py
+++ tests/test_01_list_users.py
@@ -9,21 +9,51 @@ Includes:
 - Extraction and sorting of user data
 """
 import jsonschema
-import pytest
-from utils.api_client import APIClient
 from utils.schema_loader import load_json_schema
-
-@pytest.fixture
-def api_client(base_url):
-    return APIClient(base_url)
 
 class TestListUsers:
 
-    def test_users_response_code_200(self, api_client):
-        response = api_client.get("/api/users?page=1")
+    # JSON Response Assertions
+    def test_users_response_code_200(self, response):
         assert response.status_code == 200
 
-    def test_json_schema_validate(self, api_client):
+    def test_json_schema_validate(self, response):
         schema = load_json_schema("page_1_users_schema.json")
-        response = api_client.get("/api/users?page=1")
         jsonschema.validate(response.json(), schema)
+
+    def test_page_field_is_one(self, response):
+        body = response.json()
+        assert body.get("page") == 1
+
+    def test_body_contains_6_users(self, response):
+        body = response.json()
+        users = len(body.get("data"))
+        assert users == 6
+
+    def test_first_user_name_is_george(self, response):
+        body = response.json()
+        users = body.get("data")
+        assert users[0].get("first_name") == "George"
+
+    def test_contains_name_emma(self, response):
+        body = response.json()
+        users = body.get("data")
+        assert any(user.get("first_name") == "Emma" for user in users)
+
+    def test_extract_single_user_details(self, response):
+        body = response.json()
+        users = body.get("data")
+        first_user = users[0]
+        user_id = first_user.get("id")
+        user_email = first_user.get("email")
+        assert user_id is not None
+        assert user_email is not None
+        print(f"\nUser ID: {user_id}\nUser Email: {user_email}")
+
+    def test_sort_users_by_first_name(self, response):
+        body = response.json()
+        users = body.get("data")
+        sorted_users = sorted(users, key=lambda u: u["first_name"])
+        print("\nSorted users by first name:")
+        for user in sorted_users:
+            print(f"{user['first_name']} {user['last_name']} - {user['email']}")
